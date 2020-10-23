cb5ec3867b1adf3e0e8941867b64bee378644551
Rashika Rathi <b18081@students.iitmandi.ac.in>
2020-10-23 21:06:10 +0530

    Update tkin-initial.py

--- tkin-initial.py
+++ tkin-initial.py
@@ -1,26 +1,31 @@
-from tkinter import*
+#tkinter- GUI library for python
+from tkinter import*    #importing everything from tkinter
 from tkinter import filedialog
 import numpy as np 
-from tkinter import ttk
+from tkinter import ttk #used for styling
 import matplotlib.pyplot as plt
+#creating tkinter window
 root=Tk()
 
 root.title("Software")
-root.geometry("500x750")
+# creating fixed geometry of the tkinter window with dimensions 500x750
+#.geometry("window width x window height + position right + position down")
+root.geometry("500x750+2000+100")
 
 def open_text():
-    text_file = filedialog.askopenfilename(initialdir='/home/medch',title="open text file")
+    text_file = filedialog.askopenfilename(initialdir='/home/medch',title="open text file",filetypes=(("text files","*.txt"),("all files","*.*")))
     text_file =open(text_file,'r')
     stuff=text_file.read()
-    my_text.insert(END,stuff)
+    my_text.insert(END,stuff)   #.insert(index, element)
 
     text_file.close()
     
 def save_text():
-    text_file = filedialog.askopenfilename(initialdir='/home/medch',title="open text file")
+    text_file = filedialog.askopenfilename(initialdir='/home/medch',title="open text file",filetypes=(("text files","*.txt"),("all files","*.*")))
     text_file=open(text_file,'w')
-    text_file.write(my_text.get(1.0,END))
-
+    text_file.write(my_text.get(1.0,END))   #.get(keyname, value)
+#value is Optional. A value to return if the specified key does not exist.Default value None
+    
 def countWords(lines):
   wordDict = {}
   for line in lines:
@@ -40,7 +45,7 @@ def findMostCommon(charDict):
   return mostFreq
 
 def count_lines():
-    text_file = filedialog.askopenfilename(initialdir='/home/medch',title="open text file")
+    text_file = filedialog.askopenfilename(initialdir='/home/medch',title="open text file",filetypes=(("text files","*.txt"),("all files","*.*")))
     text_file =open(text_file,'r')
     stuff=text_file.read()
     answer=0
@@ -52,7 +57,7 @@ def count_lines():
     cw=countWords(stuff)
     
     fmc=findMostCommon(cw)
-    words = len(stuff.split())
+    words = len(stuff.split())  #default separator is any whitespace
     
     Entry.insert(E4,0,answer)
     print(answer)
@@ -63,9 +68,10 @@ def count_lines():
    
     
 def plotHist():
-    text_file = filedialog.askopenfilename(initialdir='/home/medch',title="open text file")
+    text_file = filedialog.askopenfilename(initialdir='/home/medch',title="open text file",filetypes=(("text files","*.txt"),("all files","*.*")))
     text_file =open(text_file,'r')
-    stuff=text_file.read()     
+    stuff=text_file.read()  
+    # Get a tuple of unique values and their frequency in numpy array
     labels,counts = np.unique(stuff.split(),return_counts=True)
     ticks = range(len(counts))
     plt.bar(ticks,counts,align='center')
@@ -77,13 +83,14 @@ def plotHist():
 
 
 
-
+#The Text widget is used to display text in multiple lines.
 my_text=Text(root,width=20,height=20)
+"""pack is a geometry manager which organizes widgets in blocks before placing them in the parent widget."""
 my_text.pack(pady=20)
 
 L4 = Label(root, text="no of lines")
-L4.pack(pady=5)
-E4 = Entry(root, bd =5)
+L4.pack(pady=5) #pady = External padding. Default is 0.
+E4 = Entry(root, bd =5) #An Entry widget gets text input from the user.
 E4.pack(pady=5)
 L5 = Label(root, text="no of words")
 L5.pack(pady=5)
@@ -93,7 +100,7 @@ L4 = Label(root, text="most frequent words")
 L4.pack(pady=5)
 E6 = Entry(root, bd =5)
 E6.pack(pady=5)
-
+#The Button widget is used to display buttons in your application.
 open_button=Button(root,text="open text file",command=open_text)
 open_button.pack(pady=5)
 save_button=Button(root,text="save file",command=save_text)
